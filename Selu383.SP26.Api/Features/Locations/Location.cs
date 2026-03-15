96ab87ca0ab8c7ccdda46a69dc3f592341b5dd9b
IsNikhil <nikhilrksha@gmail.com>
2026-03-15 15:16:37 -0500

    location dto updated

--- Selu383.SP26.Api/Features/Locations/Location.cs
+++ Selu383.SP26.Api/Features/Locations/Location.cs
@@ -10,8 +10,22 @@ public class Location
 
     public string Address { get; set; } = string.Empty;
 
+    public string City { get; set; } = string.Empty;
+
+    public string State { get; set; } = string.Empty;
+
+    public string Zip { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public string HoursOfOperation { get; set; } = string.Empty;
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
     public int TableCount { get; set; }
 
     public int? ManagerId { get; set; }
-    public virtual User? Manager {  get; set; }
+    public virtual User? Manager { get; set; }
 }
--- Selu383.SP26.Api/Features/Locations/LocationDto.cs
+++ Selu383.SP26.Api/Features/Locations/LocationDto.cs
@@ -13,6 +13,20 @@ public class LocationDto
     [Required]
     public string Address { get; set; } = string.Empty;
 
+    public string City { get; set; } = string.Empty;
+
+    public string State { get; set; } = string.Empty;
+
+    public string Zip { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public string HoursOfOperation { get; set; } = string.Empty;
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
     public int TableCount { get; set; }
 
     public int? ManagerId { get; set; }
