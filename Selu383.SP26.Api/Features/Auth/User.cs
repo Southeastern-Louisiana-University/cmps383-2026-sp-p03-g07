2deb9459ef7044bd63ed30b89e4f2d58ad4fec0f
IsNikhil <nikhilrksha@gmail.com>
2026-03-15 16:27:18 -0500

    updated user details

--- Selu383.SP26.Api/Features/Auth/User.cs
+++ Selu383.SP26.Api/Features/Auth/User.cs
@@ -4,5 +4,11 @@ namespace Selu383.SP26.Api.Features.Auth;
 
 public class User : IdentityUser<int>
 {
+    public string? DisplayName { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+    public DateTime? Birthday { get; set; }
+    public bool BirthdayRewardClaimedYear { get; set; } = false;
+    public int? BirthdayRewardClaimedForYear { get; set; }
+
     public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 }
--- Selu383.SP26.Api/Features/Auth/UserDto.cs
+++ Selu383.SP26.Api/Features/Auth/UserDto.cs
@@ -5,4 +5,8 @@ public class UserDto
     public int Id { get; set; }
     public string UserName { get; set; } = string.Empty;
     public string[] Roles { get; set; } = Array.Empty<string>();
+    public string? DisplayName { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+    public DateTime? Birthday { get; set; }
+    public bool IsBirthday { get; set; }
 }
