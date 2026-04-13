ea61ffec633814547b4d358d7ef07d9adc07a1c7
CivPatel <pshiv4255@gmail.com>
2026-04-12 22:54:46 -0500

    mobile: use emulator-safe API base in dev

--- Selu383.SP26.Mobile/constants/api.ts
+++ Selu383.SP26.Mobile/constants/api.ts
@@ -1,6 +1,13 @@
-export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_BASE_URL ??
-  "https://selu383-sp26-p03-g07.azurewebsites.net";
+import { Platform } from 'react-native';
+
+const configuredBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL?.trim();
+const productionBaseUrl = 'https://selu383-sp26-p03-g07.azurewebsites.net';
+const devBaseUrl = Platform.select({
+  android: 'http://10.0.2.2:8080',
+  default: 'http://localhost:8080',
+});
+
+export const API_BASE_URL = (configuredBaseUrl || (__DEV__ ? devBaseUrl : productionBaseUrl)).replace(/\/$/, '');
 
 const absoluteUrlPattern = /^(?:[a-z]+:)?\/\//i;
 
